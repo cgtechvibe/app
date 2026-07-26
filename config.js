12d8638755f4e66025f58b11a2137c0c1aca10b6
CGTechVibes <cgtechvibes@gmail.com>
2026-07-27 05:55:18 +0800

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// PWA Manager - Config
-const API_URL = 'https://script.google.com/macros/s/AKfycbyx7VFXBMYSt4tTHAnf1XLkia75vUSVVjg-QoJvINpK6-fE370GBMHuD3nrZclEEOeV/exec';
-const APP_NAME = 'PWA Manager';
